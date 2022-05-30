42f68da138b102677454d203dbda71c8409f6c17
Vadim <pnevmat001@gmail.com>
2022-05-30 15:21:52 +0300

    removes console logs and commented code

--- utils/bufferDecoder/constructors/arrayConstructor.js
+++ utils/bufferDecoder/constructors/arrayConstructor.js
@@ -1,11 +1,6 @@
 const ArrayAndObjectResolver = require('../utils/ArrayAndObjectResolver');
 
 const arrayConstructor = (startI, endI, buffer) => {
-	// debugger;
-	// console.log('Buffer in array constructor and recursion: ', buffer);
-	// console.log('Start index in array constructor: ', startI);
-	// console.log('End index in array constructor: ', endI);
-	// console.log('Buffer end index in array constructor: ', buffer.length - 1);
 	const result = {endIndex: null, result: null};
 
 	let startIndex = startI;
@@ -13,24 +8,29 @@ const arrayConstructor = (startI, endI, buffer) => {
 	for (let i = startI; i <= endI; i++) {
 		if (i <= startIndex && !result.result && buffer[i] === '[') {
 			result.result = [];
+		} else if (i <= startIndex && result.result) {
+			continue;
 		} else if (
 			(i <= startIndex && !result.result && buffer[i] === '{') ||
 			(i >= startIndex && result.result && buffer[i] === '{')
 		) {
 			startIndex = i;
-			// debugger;
+
 			const resolver = new ArrayAndObjectResolver(buffer);
 			const resultOfObjectConstructor = resolver.objectConstructor(startIndex);
 			result.result.push(resultOfObjectConstructor.result);
-			// console.log('Result after push of object constructor: ', result);
 
 			startIndex = resultOfObjectConstructor.endIndex;
 			result.endIndex = startIndex;
-			break;
+
+			if (buffer.length === result.endIndex) {
+				break;
+			} else {
+				continue;
+			}
 		}
 	}
 
-	console.log('Result of arrayConstructor: ', result);
 	return result;
 };
 
--- utils/bufferDecoder/utils/ArrayAndObjectResolver.js
+++ utils/bufferDecoder/utils/ArrayAndObjectResolver.js
@@ -6,7 +6,6 @@ class ArrayAndObjectResolver {
 	}
 
 	arrayConstructor(startI) {
-		// debugger;
 		const result = {endIndex: null, result: null};
 
 		let startIndex = startI;
@@ -21,17 +20,19 @@ class ArrayAndObjectResolver {
 				(i >= startIndex && result.result && this.buffer[i] === '{')
 			) {
 				startIndex = i;
-				// debugger;
+
 				const resultOfObjectConstructor = this.objectConstructor(startIndex);
 				result.result.push(resultOfObjectConstructor.result);
-				// console.log('Result after push of object constructor: ', result);
+
 				startIndex = resultOfObjectConstructor.endIndex;
 			} else if (i >= startIndex && result.result && this.buffer[i] === '"') {
 				startIndex = i - 1;
+
 				const simpleArrayConstructorResult = simpleArrayConstructor(
 					startIndex,
 					this.buffer,
 				);
+
 				result.result = simpleArrayConstructorResult.result;
 				startIndex = simpleArrayConstructorResult.endIndex;
 			} else if (
@@ -42,13 +43,12 @@ class ArrayAndObjectResolver {
 				break;
 			}
 		}
-		console.log('Result of array constructor in class: ', result);
+
 		result.endIndex = startIndex;
 		return result;
 	}
 
 	objectConstructor(startI) {
-		// debugger;
 		const result = {endIndex: null, result: null};
 		let objectProperty = null;
 		let objectValue = null;
@@ -57,9 +57,7 @@ class ArrayAndObjectResolver {
 
 		let startIndex = startI;
 
-		// debugger;
 		for (let i = startI; i <= this.buffer.length - 1; i++) {
-			// console.log('Buffer item: ', buffer[i]);
 			if (i <= startIndex && !result.result && this.buffer[i] === '{') {
 				result.result = {};
 			} else if (i <= startIndex && result.result) {
@@ -77,10 +75,8 @@ class ArrayAndObjectResolver {
 			) {
 				if (!objectProperty) {
 					objectProperty = this.buffer[i];
-					// console.log('Object property: ', objectProperty);
 				} else {
 					objectProperty += this.buffer[i];
-					// console.log('Object property: ', objectProperty);
 				}
 			} else if (i > 1 && this.buffer[i] === ':') {
 				isObjectValue = true;
@@ -94,7 +90,6 @@ class ArrayAndObjectResolver {
 				if (i > 1 && this.buffer[i] === '[') {
 					startIndex = i;
 
-					// debugger;
 					const arrayConstructorResult = this.arrayConstructor(startIndex);
 
 					objectValue = arrayConstructorResult.result;
@@ -113,7 +108,6 @@ class ArrayAndObjectResolver {
 						continue;
 					}
 				} else if (i > 1 && this.buffer[i] === '{') {
-					// debugger;
 					startIndex = i;
 
 					const objectConstructorResult = this.objectConstructor(startIndex);
@@ -136,10 +130,8 @@ class ArrayAndObjectResolver {
 
 				if (!objectValue) {
 					objectValue = this.buffer[i];
-					// console.log('Object value: ', objectValue);
 				} else {
 					objectValue += this.buffer[i];
-					// console.log('Object value: ', objectValue);
 				}
 			} else if (
 				(i > 1 &&
@@ -149,12 +141,9 @@ class ArrayAndObjectResolver {
 					this.buffer[i + 1] === '"') ||
 				(i > 1 && this.buffer[i] === '}' && objectProperty && objectValue)
 			) {
-				// console.log('Index of iterator: ', i);
-				// console.log('Buffer end index: ', buffer.length - 1);
-				// console.log('Object property: ', objectProperty);
-				// debugger;
 				result.result[objectProperty] = objectValue;
 				result.endIndex = i;
+
 				objectProperty = null;
 				objectValue = null;
 				isObjectValue = false;
@@ -166,10 +155,11 @@ class ArrayAndObjectResolver {
 				if (this.buffer[i] === '}') {
 					result.endIndex = i;
 				}
+
 				break;
 			}
 		}
-		console.log('Result of object constructor in class: ', result);
+
 		return result;
 	}
 }
