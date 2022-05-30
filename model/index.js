c7095500df4bcab64b871afc2afb98aca8ef2292
Vadim <pnevmat001@gmail.com>
2022-05-30 14:50:26 +0300

    prepares decoder for debuging

--- model/index.js
+++ model/index.js
@@ -14,148 +14,150 @@ const getUsers = async () => {
 			},
 		);
 
-		const array = [
-			{
-				id: 'ffrf',
-				userName: 'Pnevmat',
-				fullName: 'Vadim Kravchenko',
-				lastLogin: 'Fri March 15 2022',
-				friends: [
-					{
-						id: 'hfg',
-						userName: 'solomon',
-						fullName: 'Kate Barson',
-						lastLogin: 'Fri March 15 2022',
-						activities: [
-							{
-								id: 'rerf',
-								achievements: [
-									'took part in school competitions',
-									'took part in university competitions',
-								],
-								activityName: 'Football',
-							},
-							{
-								id: 'utt7g',
-								activityName: 'voleiball',
-								achievements: [
-									'took part in school competitions',
-									'1st place on university tournament',
-									'took part in student national tournament',
-								],
-							},
-						],
-						friends: [
-							{
-								id: 'ffrf',
-								userName: 'Pnevmat',
-								fullName: 'Vadim Kravchenko',
-								lastLogin: 'Fri March 15 2022',
-								activities: [
-									{
-										id: 'fbpl',
-										achievements: [
-											'took part in school competitions',
-											'1st place on university tournament',
-											'took part in student national tournament',
-										],
-										activityName: 'Football',
-									},
-									{
-										id: 'votp',
-										activityName: 'voleiball',
-										achievements: [
-											'took part in school competitions',
-											'took part in university competitions',
-										],
-									},
-								],
-							},
-						],
-					},
-					{
-						id: 'kdd',
-						userName: 'utred',
-						fullName: 'Ken Livings',
-						lastLogin: 'Fri March 15 2022',
-						activities: [
-							{
-								id: 'aetd',
-								achievements: [
-									'took part in school competitions',
-									'took part in university competitions',
-								],
-								activityName: 'Football',
-							},
-							{
-								id: 'utryb',
-								activityName: 'voleiball',
-								achievements: [
-									'took part in school competitions',
-									'1st place on university tournament',
-									'took part in student national tournament',
-								],
-							},
-						],
-					},
-				],
-				activities: [
-					{
-						id: 'fbpl',
-						achievements: [
-							'took part in school competitions',
-							'1st place on university tournament',
-							'took part in student national tournament',
-						],
-						activityName: 'Football',
-					},
-					{
-						id: 'votp',
-						activityName: 'voleiball',
-						achievements: [
-							'took part in school competitions',
-							'took part in university competitions',
-						],
-					},
-				],
-				autorisation: {
-					login: 'pnevmat',
-					password: 'cderfv',
-					someProp: [
-						{
-							id: 'votp',
-							activityName: 'voleiball',
-							achievements: [
-								'took part in school competitions',
-								'took part in university competitions',
-							],
-						},
-						{
-							id: 'fbpl',
-							achievements: [
-								'took part in school competitions',
-								'1st place on university tournament',
-								'took part in student national tournament',
-							],
-							activityName: 'Football',
-						},
-					],
-				},
-			},
-		];
+		// const array = [
+		// 	{
+		// 		id: 'ffrf',
+		// 		userName: 'Pnevmat',
+		// 		fullName: 'Vadim Kravchenko',
+		// 		lastLogin: 'Fri March 15 2022',
+		// 		friends: [
+		// 			{
+		// 				id: 'hfg',
+		// 				userName: 'solomon',
+		// 				fullName: 'Kate Barson',
+		// 				lastLogin: 'Fri March 15 2022',
+		// 				activities: [
+		// 					{
+		// 						id: 'rerf',
+		// 						achievements: [
+		// 							'took part in school competitions',
+		// 							'took part in university competitions',
+		// 						],
+		// 						activityName: 'Football',
+		// 					},
+		// 					{
+		// 						id: 'utt7g',
+		// 						activityName: 'voleiball',
+		// 						achievements: [
+		// 							'took part in school competitions',
+		// 							'1st place on university tournament',
+		// 							'took part in student national tournament',
+		// 						],
+		// 					},
+		// 				],
+		// 				friends: [
+		// 					{
+		// 						id: 'ffrf',
+		// 						userName: 'Pnevmat',
+		// 						fullName: 'Vadim Kravchenko',
+		// 						lastLogin: 'Fri March 15 2022',
+		// 						activities: [
+		// 							{
+		// 								id: 'fbpl',
+		// 								achievements: [
+		// 									'took part in school competitions',
+		// 									'1st place on university tournament',
+		// 									'took part in student national tournament',
+		// 								],
+		// 								activityName: 'Football',
+		// 							},
+		// 							{
+		// 								id: 'votp',
+		// 								activityName: 'voleiball',
+		// 								achievements: [
+		// 									'took part in school competitions',
+		// 									'took part in university competitions',
+		// 								],
+		// 							},
+		// 						],
+		// 					},
+		// 				],
+		// 			},
+		// 			{
+		// 				id: 'kdd',
+		// 				userName: 'utred',
+		// 				fullName: 'Ken Livings',
+		// 				lastLogin: 'Fri March 15 2022',
+		// 				activities: [
+		// 					{
+		// 						id: 'aetd',
+		// 						achievements: [
+		// 							'took part in school competitions',
+		// 							'took part in university competitions',
+		// 						],
+		// 						activityName: 'Football',
+		// 					},
+		// 					{
+		// 						id: 'utryb',
+		// 						activityName: 'voleiball',
+		// 						achievements: [
+		// 							'took part in school competitions',
+		// 							'1st place on university tournament',
+		// 							'took part in student national tournament',
+		// 						],
+		// 					},
+		// 				],
+		// 			},
+		// 		],
+		// 		activities: [
+		// 			{
+		// 				id: 'fbpl',
+		// 				achievements: [
+		// 					'took part in school competitions',
+		// 					'1st place on university tournament',
+		// 					'took part in student national tournament',
+		// 				],
+		// 				activityName: 'Football',
+		// 			},
+		// 			{
+		// 				id: 'votp',
+		// 				activityName: 'voleiball',
+		// 				achievements: [
+		// 					'took part in school competitions',
+		// 					'took part in university competitions',
+		// 				],
+		// 			},
+		// 		],
+		// 		autorisation: {
+		// 			login: 'pnevmat',
+		// 			password: 'cderfv',
+		// 			someProp: [
+		// 				{
+		// 					id: 'votp',
+		// 					activityName: 'voleiball',
+		// 					achievements: [
+		// 						'took part in school competitions',
+		// 						'took part in university competitions',
+		// 					],
+		// 				},
+		// 				{
+		// 					id: 'fbpl',
+		// 					achievements: [
+		// 						'took part in school competitions',
+		// 						'1st place on university tournament',
+		// 						'took part in student national tournament',
+		// 					],
+		// 					activityName: 'Football',
+		// 				},
+		// 			],
+		// 		},
+		// 	},
+		// ];
 
-		const buffer = Buffer.from(JSON.stringify(array), 'utf-8');
+		// const buffer = Buffer.from(JSON.stringify(array), 'utf-8');
+
 		// console.log('Training buffer: ', buffer);
 		// console.log('Type of training buffer', typeof buffer);
 
-		const stringBuffer = buffer.toString();
+		// const stringBuffer = buffer.toString();
+
 		// console.log('Stringified buffer in index.js: ', stringBuffer);
 		// console.log(
 		// 	'Type of stringified buffer in index.js: ',
 		// 	typeof stringBuffer,
 		// );
 
-		const decodedBuffer = bufferDecoder(stringBuffer);
+		const decodedBuffer = bufferDecoder(db);
 		console.log('Decoded buffer: ', decodedBuffer);
 		return decodedBuffer;
 	} catch (e) {
